27b92f5ee101111c0c0670c0e09e3eb582ffe944
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-24 13:18:26 -0500

    Add Request folder path for Post and Users tests.

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'rails-controller-testing'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' do
+    before(:example) { get '/users/:user_id/posts' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include("Username: Comment 1")
+    end
+  end
+
+  describe 'GET #show', type: :request do
+  before(:example) { get '/users/:user_id/posts/:id' }
+
+  it 'is a success' do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "renders 'index' template" do
+    expect(response).to render_template('show')
+  end
+
+  it 'response body includes correct placeholder text' do
+    expect(response.body).to include("Post 1 by username")
+  end
+  end
+end
