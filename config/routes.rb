8c2dab9fb568fa4b972cd1eef83e759c018d3602
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-24 13:15:26 -0500

    Add indications of how to implement routes for future references.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
+  
+  # We can do this either with get or with resources
+  # get 'users', to: 'users#index'
+  # get 'users/:id', to: 'users#show'
+  # get 'users/:user_id/posts', to: 'posts#index'
+  # get 'users/:user_id/posts/:id', to: 'posts#show'
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
