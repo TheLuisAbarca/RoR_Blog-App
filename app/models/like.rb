90e1db232cfb0ce5afe603a5cc5f85bb9c5b2051
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-29 12:59:39 -0500

    Implement Like model with asked specifications.

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :user, foreign_key: 'users_id'
+  belongs_to :post, foreign_key: 'posts_id'
+  after_save :likes_counter
+
+  def likes_counter
+    post = Post.find(posts_id)
+    post.increment!(:likesCounter)
+  end
+end
