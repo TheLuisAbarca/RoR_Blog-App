8c63dca02b16d5ff156d9b8031638d7352d65142
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-29 13:05:16 -0500

    Add the current_user method to the application_controller.rb file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.first
+    # This could serve when we implement authentication
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
