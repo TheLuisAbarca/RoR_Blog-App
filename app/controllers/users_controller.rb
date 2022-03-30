3dacc6204390cdcdefaf137bcc52659f4890fcc3
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-30 17:22:38 -0500

    Implement the controllers part with the necesary asignation of instance variables to be used on views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  def index; end
+  def index 
+    @users = User.order(:id)
+  end
 
-  def show; end
+  def show 
+    @user = User.find(params[:id])
+    # @posts = @user.posts.order(:id)
+    # @posts = Post.where(author_id: params[:id]).order(:id)
+    @posts = Post.where(author_id: @user.id).order(:id)
+  end
 end
