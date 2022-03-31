f30807ae3aa796025bc6188c986387066d9c810a
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-31 15:29:06 -0500

    Update and Fix mispells in column names of DB.

--- db/migrate/20220329205112_rename_author_columns.rb
+++ db/migrate/20220329205112_rename_author_columns.rb
@@ -1,7 +1,7 @@
 class RenameAuthorColumns < ActiveRecord::Migration[7.0]
   def change
-    rename_column :posts, :users_id, :author_id
-    rename_column :comments, :users_id, :author_id
-    rename_column :likes, :users_id, :author_id
+    rename_column :posts, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
+    rename_column :likes, :user_id, :author_id
   end
 end
