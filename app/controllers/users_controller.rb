33556dc0d379af10fdfaf1997a0c7020e079fbae
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-30 19:53:17 -0500

    Delete commented code and fixed the asignation of instance variable to User.all .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id)
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    # @posts = @user.posts.order(:id)
-    # @posts = Post.where(author_id: params[:id]).order(:id)
     @posts = Post.where(author_id: @user.id).order(:id)
   end
 end
