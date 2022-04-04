100bda472a31ac54a6aafb11201d49cbf9397d21
Lucas-Masaba <masabaluke@gmail.com>
2022-04-05 02:12:57 +0300

    try to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,25 @@ class ApplicationController < ActionController::Base
     #User.first
     # This could serve when we implement authentication
     # @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    protect_from_forgery prepend: true
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
+    # protect_from_forgery prepend: true
+    # before_action :configure_permitted_parameters, if: :devise_controller?
+    # before_action :authenticate_user!
   
+    # protected
+  
+    # def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # end
+    protect_from_forgery with: :exception
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
     protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(name:, bio:, photo:, email:, password:) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(name:, bio:, photo:, email:, password:, current_password:) }
+
     end
   end
 end
