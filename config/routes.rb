6b51db14ebf249e3182375251a2cefc79b6676bd
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-04 15:25:28 -0500

    Add routes for devise users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
   # We can do this either with get or with resources
   # get 'users', to: 'users#index'
