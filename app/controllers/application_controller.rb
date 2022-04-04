3317090c260e12c1e0cbfa664a55f5c84a4d31c9
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-04 15:37:56 -0500

    Add basic configuration of authentication on application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   def current_user
-    User.first
+    #User.first
     # This could serve when we implement authentication
     # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    protect_from_forgery prepend: true
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
   end
 end
